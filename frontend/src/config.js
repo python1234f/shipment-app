848c1d9ae7fe5b5b4e8fc18971c7c5246f0859fa
Your Name <you@example.com>
2023-05-30 22:16:41 +0200

    fix for bug

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,4 @@
-//export const BASE_API_URL = "http://localhost:8081/api/"
-export const BASE_API_URL = "http://localhost:8000/api/"
+export const BASE_API_URL = "http://localhost:8081/api/"
 
 export const allocationsList = {
     url: "allocations"
